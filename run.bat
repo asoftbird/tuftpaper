740015b1b97964a489613dc4810c6b75b264b106
asoftbird <asoftbird@gmail.com>
2022-08-19 17:12:30 +0200

    added toast notif because that's neat too

--- run.bat
+++ run.bat
@@ -2,3 +2,4 @@ call %~dp0\venv\Scripts\python.exe main.py
 powershell -nop -c "& {sleep 10}"
 RUNDLL32.EXE USER32.dll, UpdatePerUserSystemParameters
 RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters 1, True
+powershell -File .\toast.ps1 "Tuftie posted!"
