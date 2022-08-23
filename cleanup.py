56eea1bbeccc99931d45ffa81c9030ecc19dc3f1
asoftbird <asoftbird@gmail.com>
2022-08-23 14:47:42 +0200

    cleaned up

--- cleanup.py
+++ cleanup.py
@@ -1,17 +1,11 @@
-import re
 import os
-import sys
 import shutil
 import json
 import logging
 from datetime import date
 
-
-# config
-# TODO: pull this from exec arguments instead of json
 with open("config.json") as cfgfile:
     CFG = json.load(cfgfile)
-
 
 LOGFILE=CFG["log-file-cleanup"]
 OUTPUT_DIR=CFG["output-dir"]
@@ -30,15 +24,23 @@ month = str(current_date.strftime("%b")).zfill(2)
 date_today = day + "-" + month
 date_yesterday = str(int(day)-1) + "-" + month
 
-files_to_move = []
-
-print(f"Today: {date_today}, yesterday: {date_yesterday}")
+# check if there's enough pictures for today. if not: leave yesterday's images in place
+files_today = 0
+for filename in os.listdir(OUTPUT_DIR):
+    if filename.startswith(date_today):
+        files_today += 1
 
 counter = 0
-for filename in os.listdir(OUTPUT_DIR):  
-    if not filename.startswith(date_today) and not filename.startswith(date_yesterday):
-        counter += 1
-        shutil.move(os.path.join(OUTPUT_DIR, filename), os.path.join(ARCHIVE_DIR, filename))
-        logging.info(f"Moved {filename} to archive.")
-
-logging.info(f"Moved {counter} files.")
+for filename in os.listdir(OUTPUT_DIR): 
+    if files_today <= 8: 
+        if not filename.startswith(date_today) and not filename.startswith(date_yesterday):
+            counter += 1
+            shutil.move(os.path.join(OUTPUT_DIR, filename), os.path.join(ARCHIVE_DIR, filename))
+            #logging.info(f"Moved {filename} to archive.")
+    else:
+        if not filename.startswith(date_today):
+            counter += 1
+            shutil.move(os.path.join(OUTPUT_DIR, filename), os.path.join(ARCHIVE_DIR, filename))
+            #logging.info(f"Moved {filename} to archive.")
+    
+logging.info(f"Moved {counter} files to archive.")
