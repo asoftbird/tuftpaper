02942fc133c475d820640015b61e0280d5a049e9
asoftbird <asoftbird@gmail.com>
2022-08-23 14:40:23 +0200

    added script to archive older images

---
+++ cleanup.py
@@ -0,0 +1,44 @@
+import re
+import os
+import sys
+import shutil
+import json
+import logging
+from datetime import date
+
+
+# config
+# TODO: pull this from exec arguments instead of json
+with open("config.json") as cfgfile:
+    CFG = json.load(cfgfile)
+
+
+LOGFILE=CFG["log-file-cleanup"]
+OUTPUT_DIR=CFG["output-dir"]
+ARCHIVE_DIR=CFG["archive-dir"]
+
+logging.basicConfig(filename=LOGFILE, 
+    encoding='utf-8', 
+    level=logging.INFO, 
+    format='%(asctime)s %(message)s',
+    datefmt='[%a %d-%m-%y %H:%M:%S]'
+    )
+
+current_date = date.today()
+day = str(current_date.day).zfill(2)
+month = str(current_date.strftime("%b")).zfill(2)
+date_today = day + "-" + month
+date_yesterday = str(int(day)-1) + "-" + month
+
+files_to_move = []
+
+print(f"Today: {date_today}, yesterday: {date_yesterday}")
+
+counter = 0
+for filename in os.listdir(OUTPUT_DIR):  
+    if not filename.startswith(date_today) and not filename.startswith(date_yesterday):
+        counter += 1
+        shutil.move(os.path.join(OUTPUT_DIR, filename), os.path.join(ARCHIVE_DIR, filename))
+        logging.info(f"Moved {filename} to archive.")
+
+logging.info(f"Moved {counter} files.")
